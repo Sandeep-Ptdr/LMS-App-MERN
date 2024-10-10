893459a76e8051a6401a7a87ca5ae563d00a7d35
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-10-10 15:09:01 +0530

    add course schema

---
+++ server/src/models/course.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required."],
+  },
+  description: {
+    type: String,
+  },
+  instructor: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  }],
+  content: {
+    type: String,
+    required: [true, "Content is required."],
+  },
+  enrolledStudents: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  status: {
+    type: String,
+    enum: ["draft", "published"],
+    default: "draft",
+  },
+},{timestamps: true});
+
+const Course = mongoose.model('course',courseSchema);
+export default Course;
+
