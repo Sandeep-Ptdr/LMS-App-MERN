f82196e2516f406dad76564c94db97738556f3a0
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-10-12 14:09:07 +0530

    add: quiz submit route

--- server/src/routes/quizRoutes.js
+++ server/src/routes/quizRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { authenticateToken, checkInstructorRole } from "../middlewares/authMiddleware.js";
-import { createQuiz,getAllQuizzes, getSingleQuiz } from "../controllers/quizContoller.js";
+import { createQuiz,deleteQuiz,getAllQuizzes, getSingleQuiz, submitQuiz } from "../controllers/quizContoller.js";
  
 
 const router = Router();
 
-router.post("/:lessonId/quiz", authenticateToken, checkInstructorRole, createQuiz);
-router.get("/:lessonId/quizzes", authenticateToken, getAllQuizzes)
-router.get("/quiz/:quizId", authenticateToken,getSingleQuiz)
+router.post("/:lessonId/quiz", authenticateToken, checkInstructorRole, createQuiz); // create quiz
+router.get("/:lessonId/quizzes", authenticateToken, getAllQuizzes)// get all quizzes
+router.get("/quiz/:quizId", authenticateToken,getSingleQuiz)//get single quiz
+router.delete('/delete/quiz/:quizId', authenticateToken, checkInstructorRole, deleteQuiz);//delet quiz
+router.post("/quiz/:quizId/submit", authenticateToken, submitQuiz);//for sumit quiz
 export default router;
