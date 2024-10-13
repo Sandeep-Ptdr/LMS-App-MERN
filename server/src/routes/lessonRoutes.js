8091cd159adb9d27563969364eeaedd87bedae4c
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-10-13 14:50:32 +0530

    api update

--- server/src/routes/lessonRoutes.js
+++ server/src/routes/lessonRoutes.js
@@ -4,13 +4,13 @@ import { createLesson, deleteLesson, getAllLessons, getSingleLesson, updateLesso
 
 const router = Router();
 
-router.post("/course/:courseId/lesson", authenticateToken, checkInstructorRole, createLesson);//create a lesson for course by instructor only.
+router.post("/course/:courseId/lesson/create", authenticateToken, checkInstructorRole, createLesson);//create a lesson for course by instructor only.
 
 router.get("/course/:courseId/lessons", authenticateToken, getAllLessons);//get all lesson by anyone
 
 router.get("/lesson/:lessonId", authenticateToken, getSingleLesson);//get a specific lesson
 
-router.put("/lesson/:lessonId",authenticateToken, checkInstructorRole, updateLesson)//update a lesson by instructor only.
+router.put("/lesson/:lessonId/update",authenticateToken, checkInstructorRole, updateLesson)//update a lesson by instructor only.
 
-router.delete("/lesson/:lessonId", authenticateToken, checkInstructorRole, deleteLesson)//delete a lesson by instructor
+router.delete("/lesson/:lessonId/delete", authenticateToken, checkInstructorRole, deleteLesson)//delete a lesson by instructor
 export default router;
