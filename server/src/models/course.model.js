5739ed15b9cdd744bba03a1f5def1a0c0c05955a
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-10-15 21:47:19 +0530

    update code

--- server/src/models/course.model.js
+++ server/src/models/course.model.js
@@ -1,43 +1,48 @@
 import mongoose from "mongoose";
 
-const courseSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Title is required."],
-  },
-  description: {
-    type: String,
-  },
-  instructor: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  }],
-  content: {
-    type: String,
-    required: [true, "Content is required."],
-  },
-  enrolledStudents: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+const courseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required."],
     },
-  ],
-  price:{
-    type: Number,
-    require: true
-  },
-  status: {
-    type: String,
-    enum: ["draft", "published"],
-    default: "draft",
-  },
-  averageRating: {type: Number, default: 0},
-  category:{
-    type: String,
-    required: true
-  }
-},{timestamps: true});
+    description: {
+      type: String,
+    },
+    instructor: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    content: {
+      type: String,
+      required: [true, "Content is required."],
+    },
+    enrolledStudents: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    price: {
+      type: Number,
+      require: true,
+    },
+    status: {
+      type: String,
+      enum: ["draft", "published"],
+      default: "draft",
+    },
+    averageRating: { type: Number, default: 0 },
 
-const Course = mongoose.model('course',courseSchema);
+    category: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Course = mongoose.model("course", courseSchema);
 export default Course;
-
