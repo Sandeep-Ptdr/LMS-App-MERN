11d0c6381bb08f01ebc712225812481cc456f0ed
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-10-17 22:05:30 +0530

    new addons

---
+++ client/src/components/sidebars/Student.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Student = () => {
+  return (
+     <aside>
+        <ul>
+            <li>Dashboard</li>
+            <li>Browse Course</li>
+            <li>My Courses</li>
+            <li>Take Quiz</li>
+            <li>Quiz Results</li>
+            
+        </ul>
+     </aside>
+  )
+}
+
+export default Student
