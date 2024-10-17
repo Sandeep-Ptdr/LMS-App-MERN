330a545596567f64d82912a74a4088b488fde182
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-10-17 21:58:33 +0530

    add: routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
+import Register from './pages/auth/Register.jsx'
+import Login from './pages/auth/Login.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+     <Route path='/' element={<App/>}>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+
+      
+     </Route>
+  )
+)
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
