e2db2ce325da7c6cc3ce7af5cbcab6a8df20b29b
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-10-18 20:09:20 +0530

    Add: routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,15 +5,26 @@ import './index.css'
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import Register from './pages/auth/Register.jsx'
 import Login from './pages/auth/Login.jsx'
+import Student from './components/sidebars/Student.jsx'
+import Instructor from './components/sidebars/Instructor.jsx'
+import Header from './components/common/Header.jsx'
+import InstructorLayout from './layouts/InstructorLayout.jsx'
+import InstructorDashboard from './pages/dashboard/InstructorDashboard.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
      <Route path='/' element={<App/>}>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/login' element={<Login/>}/>
+        <Route path='register' element={<Register/>}/>
+        <Route path='login' element={<Login/>}/>
+
+
+        <Route path='instructor/dashboard' element={<InstructorLayout/>}>
+           <Route index element={<InstructorDashboard/>}/>
+
+        </Route>
+     </Route>
 
       
-     </Route>
   )
 )
 
