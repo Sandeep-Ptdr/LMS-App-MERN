ad981edb9cd74515c29f2e2ac3c28b8b5c903394
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-10-22 01:04:10 +0530

    Add: icons

--- client/src/components/dashboard/instructor/Overview.jsx
+++ client/src/components/dashboard/instructor/Overview.jsx
@@ -1,45 +1,53 @@
-import React from 'react'
+import React from "react";
+import { PiStudentBold } from "react-icons/pi";
+import { GiBookshelf } from "react-icons/gi";
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
 
-const Overview = () => {    
+
+const Overview = () => {
   return (
-     <>
+    <>
       <h1 className="font-semibold text-2xl text-gray-700 mb-4">
-            Welcome Instructor
-            </h1>
-        
-            <div className="card-container flex flex-wrap gap-5">
-                <div className="border-l-[3px] bg-gray-100 border-[#2196F3] w-56 h-28 rounded-xl shadow-lg  p-1 flex flex-col  flex-grow">
-                    <h6 className="text-gray-600 font-semibold text-center">
-                    Total Students
-                    </h6>
+        Welcome Instructor
+      </h1>
 
-                    <div className="w-full h-full flex justify-center items-center">
-                        <p className="text-3xl text-gray-700">1000</p>
-                    </div>
-                </div>
+      <div className="card-container flex flex-wrap gap-5">
+        <div className="border-l-[3px] bg-gray-100 border-[#2196F3] w-56 h-28 rounded-xl shadow-lg  p-1 flex  flex-grow items-center justify-center gap-4">
+          <div>
+            <PiStudentBold className="w-11 h-11 text-[#2196F3]" />
+          </div>
 
-                <div className="border-l-[3px] bg-gray-100 border-red-500 w-56 h-28 rounded-xl shadow-lg  p-1 flex flex-col  flex-grow">
-                    <h6 className="text-gray-600 font-semibold text-center">
-                    Total Courses
-                    </h6>
+          <div>
+            <h6 className="text-gray-600 font-semibold">Total Students</h6>
+            <p className="text-3xl text-gray-700">1000</p>
+          </div>
+        </div>
 
-                    <div className="w-full h-full flex justify-center items-center">
-                        <p className="text-3xl text-gray-700">50</p>
-                    </div>
-                </div>
+        <div className="border-l-[3px] bg-gray-100 border-red-500 w-56 h-28 rounded-xl shadow-lg  p-1 flex  flex-grow items-center justify-center gap-4">
+          <div>
+            <GiBookshelf className="w-11 h-11 text-red-500" />
+          </div>
 
-                <div className="border-l-[3px] bg-gray-100 border-green-600 w-56 h-28 rounded-xl shadow-lg  p-1 flex flex-col flex-grow">
-                    <h6 className="text-gray-600 font-semibold text-center">
-                    Total Earnings
-                    </h6>
+          <div>
+            <h6 className="text-gray-600 font-semibold">Total Courses</h6>
+            <p className="text-3xl text-gray-700">1000</p>
+          </div>
+        </div>
 
-                    <div className="w-full h-full flex justify-center items-center">
-                        <p className="text-3xl text-gray-700">1000</p>
-                    </div>
-                </div>
-            </div>
-     </>
-  )
-}
+        <div className="border-l-[3px] bg-gray-100 border-green-500 w-56 h-28 rounded-xl shadow-lg  p-1 flex  flex-grow items-center justify-center gap-4">
+          <div>
+            <RiMoneyDollarCircleLine className="w-11 h-11 text-green-500" />
+          </div>
 
-export default Overview
+          <div>
+            <h6 className="text-gray-600 font-semibold">Total Earnings</h6>
+            <p className="text-3xl text-gray-700">1000</p>
+          </div>
+        </div>
+
+      </div>
+    </>
+  );
+};
+
+export default Overview;
