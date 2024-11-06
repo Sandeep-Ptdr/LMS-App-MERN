48749f627fc47f70bfbfd52a7a8fbd178956b184
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-11-06 22:48:35 +0530

    new route

--- server/src/routes/lessonRoutes.js
+++ server/src/routes/lessonRoutes.js
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import { authenticateToken, checkInstructorRole } from "../middlewares/authMiddleware.js";
 import { createLesson, deleteLesson, getAllLessons, getSingleLesson, updateLesson } from "../controllers/lessonController.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.post("/course/:courseId/lesson/create", authenticateToken, checkInstructorRole, createLesson);//create a lesson for course by instructor only.
+router.post("/course/:courseId/lesson/create", authenticateToken, checkInstructorRole, upload.fields([{name: "video"}, {name: "pdf"}, {name: "image"}]), createLesson);//create a lesson for course by instructor only.
 
 router.get("/course/:courseId/lessons", authenticateToken, getAllLessons);//get all lesson by anyone
 
 router.get("/lesson/:lessonId", authenticateToken, getSingleLesson);//get a specific lesson
+router.get("/lesson/:lessonId/edit", authenticateToken,checkInstructorRole, getSingleLesson);//get a specific lesson by instructor only
 
-router.put("/lesson/:lessonId/edit",authenticateToken, checkInstructorRole, updateLesson)//update a lesson by instructor only.
+router.put("/lesson/:lessonId/edit",authenticateToken, checkInstructorRole,upload.fields([{ name: "video" }, { name: "pdf" }, {name: "image"}]), updateLesson)//update a lesson by instructor only.
 
 router.delete("/lesson/:lessonId/delete", authenticateToken, checkInstructorRole, deleteLesson)//delete a lesson by instructor
 export default router;
