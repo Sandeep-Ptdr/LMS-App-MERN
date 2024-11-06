8f9ac9f65e57e5330c5c2dffe275ffd89f3f86b0
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-11-06 22:45:36 +0530

    first commit

--- client/src/components/dashboard/instructor/course/EditLesson.jsx
+++ client/src/components/dashboard/instructor/course/EditLesson.jsx
@@ -1,8 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useFetchData from '../../../../hooks/useFetchData';
+import { useParams } from 'react-router-dom';
 
 const EditLesson = () => {
+  const params = useParams();
+  const [editData, setEditData] = useState({
+    title: "", 
+    description: ""
+  })
+  const [videoUrl, setVideoUrl] = useState(null);
+  const [pdfUrl, setPdfUrl] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
 
+  const {data, loading, error, fetchData} = useFetchData();
 
+  const handleFileChange = (e) => {
+    if (e.target.name === 'video'){
+      setVideoUrl(e.target.files[0]);
+    } else if (e.target.name === 'pdf'){
+      setPdfUrl(e.target.files[0]);
+    } else if (e.target.name === 'image'){
+      setImageUrl(e.target.files[0]);
+    }
+  }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditData({ ...editData, [name]: value });
+     
+  }
+
+  const handleSave =  () => {
+    const formData = new FormData();
+
+    formData.append('title', editData.title);
+    formData.append('description', editData.description);
+    if(videoUrl) formData.append('video', videoUrl);
+    if(pdfUrl) formData.append('pdf', pdfUrl);
+    if(imageUrl) formData.append('image', imageUrl);
+   
+    
+    fetchData(`/instructor/lesson/${params.lessonId}/edit`, "PUT", formData)
+     
+  }
+
+  useEffect(() => {
+    fetchData(`/instructor/lesson/${params.lessonId}/edit`, "GET")
+  }, [])
+  
+  useEffect(() => {
+
+    if(loading){
+      <p>Loading...</p>
+      return;
+    }
+
+    if (error) {
+      <p>{error?.data?.message}</p>
+      return;
+      
+    }
+
+    if(data){
+       setEditData({
+        title: data?.lesson?.title,
+        description: data?.lesson?.description
+       })
+       setVideoUrl(data?.lesson?.videoUrl)
+       setPdfUrl(data?.lesson?.pdfUrl)
+       setImageUrl(data?.lesson?.imageUrl)  
+
+    }
+
+  },[data])
+
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>{error?.data?.message && error?.data?.error}</p>
+  
     
   return (
     <div className="container mx-auto px-4">
@@ -20,10 +94,10 @@ const EditLesson = () => {
             <input
               type="text"
               name="title"
-              // value={editData.title || " "}
-              // onChange={handleInputChange}
+              value={editData.title}
+              onChange={handleInputChange}
               className="w-full outline-none px-3 py-2 border border-gray-300 rounded focus:border-[#2196F3]"
-              placeholder="Enter course title"
+              placeholder="Enter lesson title"
             />
           </div>
 
@@ -33,11 +107,11 @@ const EditLesson = () => {
             </label>
             <textarea
               name="description"
-              // value={editData.description || " "}
-              // onChange={handleInputChange}
+              value={editData.description}
+              onChange={handleInputChange}
               className="w-full px-3 py-2 border border-gray-300 rounded outline-none focus:border-[#2196F3]"
               rows="4"
-              placeholder="Enter course description"
+              placeholder='Enter lesson description'  
             ></textarea>
           </div>
 
@@ -49,12 +123,12 @@ const EditLesson = () => {
               name="image"
               className="w-full px-3 py-2 border border-gray-300 rounded outline-none focus:border-[#2196F3]"
               accept="image/*"
-              // onChange={handleImageUpload}
+              onChange={handleFileChange}
             />
           </div>
 
           <button
-            // onClick={handleSave}
+            onClick={handleSave}
             className="bg-[#2196F3] hover:bg-[#3286cb] text-white font-bold py-2 px-4 rounded"
           >
             Save Changes
@@ -72,7 +146,7 @@ const EditLesson = () => {
               className="w-full h-36 border border-gray-300 rounded"
               controls
             >
-              <source src="https://example.com" type="video/mp4" />
+              <source src={videoUrl} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
@@ -84,7 +158,7 @@ const EditLesson = () => {
               name="video"
               className="w-full px-3 py-2 border border-gray-300 rounded"
               accept="video/*"
-              // onChange={handleVideoUpload}
+              onChange={handleFileChange}
             />
           </div>
 
@@ -94,18 +168,14 @@ const EditLesson = () => {
             <input
               type="file"
               name="pdf"
+
               className="w-full px-3 py-2 border border-gray-300 rounded"
               accept="application/pdf"
-              // onChange={handlePDFUpload}
+              onChange={handleFileChange}
             />
           </div>
 
-          <button
-            // onClick={handleVideoUrl}
-            className="bg-[#2196F3] hover:bg-[#3286cb] text-white font-bold py-2 px-4 rounded"
-          >
-            Save Video and PDF
-          </button>
+           
         </div>
       </div>
     </div>
