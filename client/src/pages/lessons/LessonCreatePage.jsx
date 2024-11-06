4c3b56aa2bbfe15622bf8eb12755f0ba62b9c0f8
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-11-06 22:46:57 +0530

    new lesson create page

---
+++ client/src/pages/lessons/LessonCreatePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CreateLesson from '../../components/dashboard/instructor/course/CreateLesson'
+
+const LessonCreatePage = () => {
+  return (
+    <div className="w-full p-4">
+    <div className=" max-w-[95%] m-auto">
+      <CreateLesson/>
+    </div>
+  </div>
+  )
+}
+
+export default LessonCreatePage
