b43e656088a5062fa500dac3f0dadebf2f837454
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-11-08 21:03:05 +0530

    add btns

--- client/src/components/dashboard/instructor/course/InstructorCourseCard.jsx
+++ client/src/components/dashboard/instructor/course/InstructorCourseCard.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { PiDotsThreeOutlineVertical } from "react-icons/pi";
+import useFetchData from "../../../../hooks/useFetchData";
 
-const InstructorCourseCard = ({course}) => {
+const InstructorCourseCard = ({course, onDelete, onPublish}) => {
+
+  const [dropDownOpen, setDropDownOpen] = useState(false)
+
+
+
+  const toggleDropDown = () => {
+    setDropDownOpen((prev) => !prev);
+  }
+
+
+   
+
   return (
-    <div className="w-72 rounded overflow-hidden shadow-lg bg-gray-50">
+    <div className="w-72 rounded overflow-hidden shadow-lg bg-gray-50 relative">
+      <div className="absolute top-2 right-2 bg-gray-700/25 p-1 rounded-full z-10 cursor-pointer  "
+      onClick={toggleDropDown}>
+        <PiDotsThreeOutlineVertical className="text-gray-50"/>
+        
+      </div>
+
+      {dropDownOpen && (
+        <div className="absolute top-10 right-2 bg-gray-50 shadow-md rounded p-2 z-20 w-24">
+          <button 
+            className="w-full text-left px-2 py-1 text-red-600 hover:bg-red-100 rounded"
+            onClick={() => onDelete(course._id)}
+          >
+            Delete
+          </button>
+          <button 
+            className="w-full text-left px-2 py-1 text-[#3da8ff] hover:bg-blue-100 rounded mt-1"
+             onClick={() => onPublish(course._id)}
+          >
+            Publish
+          </button>
+        </div>
+      )}
       <div className="overflow-hidden">
         <img
-          className="w-full h-36 object-cover hover:scale-105 transition-all duration-200 ease-in-out"
+          className="w-full h-36 object-cover hover:scale-105 transition-all duration-300 ease-in-out"
           src={course?.image || "https://miro.medium.com/v2/resize:fit:1200/1*y6C4nSvy2Woe0m7bWEn4BA.png"}
           alt="Course Image"
         />
