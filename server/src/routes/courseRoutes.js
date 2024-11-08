6ab43734c1a83a61e63a0f191eeae924bb74f67f
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-11-08 21:04:33 +0530

    add new route

--- server/src/routes/courseRoutes.js
+++ server/src/routes/courseRoutes.js
@@ -5,6 +5,7 @@ import {
 } from "../middlewares/authMiddleware.js";
 import {
   createCourse,
+  deleteCourse,
   enrollInCourse,
   getAllCourse,
   getCourse,
@@ -22,14 +23,16 @@ router.get("/course/:courseId", authenticateToken, getCourse); //get course if s
 router.get("/course/:courseId/edit", authenticateToken,checkInstructorRole, getCourse); //get course by instructor 
 router.get("/course/:courseId/detail", authenticateToken,checkInstructorRole, getCourse); //get course by instructor 
 router.post("/course/:courseId/enroll", authenticateToken, enrollInCourse); //enroll students in course
-router.post(
-  "/:courseId/publishcourse",
+router.put(
+  "/:courseId/publish",
   authenticateToken,
   checkInstructorRole,
   publishCourse
 );//only instructor publish the course
 
 router.put('/course/:courseId/edit',authenticateToken,checkInstructorRole,updateCourse);//course should be update by only instructor
+
+router.delete(`/course/:courseId/delete`, authenticateToken, checkInstructorRole, deleteCourse);
 
 router.post('/:courseId/rate',authenticateToken,submitRating);// for giving rating
 export default router;
