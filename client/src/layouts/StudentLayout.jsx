bd7aa4a87af73ac8fc5690d61741d285d82160f3
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-11-09 21:34:29 +0530

    student layout

--- client/src/layouts/StudentLayout.jsx
+++ client/src/layouts/StudentLayout.jsx
@@ -1,10 +1,24 @@
  import React from 'react'
+import Student from '../components/sidebars/Student'
+import Header from '../components/common/Header'
+import StudentDashboard from '../pages/dashboard/studentDashboard/StudentDashboard'
  
  const StudentLayout = () => {
    return (
-     <header>
-        
-     </header>
+    <>
+    <div className="flex flex-col h-screen">
+      <Header />
+      <div className="flex flex-1 overflow-hidden">
+      {/* sidebar */}
+        <Student /> 
+
+        <main className="flex flex-1 overflow-y-auto w-[calc(100vw-256px)] bg-gray-200">
+          {/* <Outlet /> */}
+           <StudentDashboard />
+        </main>
+      </div>
+    </div>
+  </>
    )
  }
  
