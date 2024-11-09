dc59153f81548394bde689843a1663f3847309f2
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-11-09 21:33:31 +0530

    student dashboard

---
+++ client/src/components/dashboard/student/DashboardOverview.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import ProgressChart from "../../charts/ProgressChart";
+import DashboardCourseCard from "./courses/DashboardCourseCard";
+import DashboardQuizDetails from "./courses/DashboardQuizDetails";
+
+const DashboardOverview = () => {
+  return (
+    <>
+      <h1 className="font-semibold text-2xl text-gray-700 mb-4">Welcome</h1>
+      <div className="flex justify-between">
+        <div className="mb-4 flex-1 ">
+          <ProgressChart />
+        </div>
+        <div className=" bg-gray-50 rounded-md flex-1 h-[365px] overflow-hidden">
+          <div className="flex justify-between items-center p-4 border-b border-gray-300">
+            <h1 className="text-2xl font-semibold text-gray-600">Quizzes</h1>
+            <button className="px-3 py-2 bg-[#2196F3] text-gray-50 font-semibold rounded-md hover:bg-[#1976D2]">
+              Quiz Results
+            </button>
+          </div>
+          <div className="overflow-y-scroll h-[calc(100%-72px)] scroll-smooth ">
+            <DashboardQuizDetails/>
+            <DashboardQuizDetails/>
+            <DashboardQuizDetails/>
+            <DashboardQuizDetails/>
+            <DashboardQuizDetails/>
+            <DashboardQuizDetails/>
+            <DashboardQuizDetails/>
+             
+           
+             
+          </div>
+        </div>
+      </div>
+
+      <div className=" bg-gray-50 rounded">
+        <div className=" flex justify-between items-center p-4 border-b border-gray-300">
+          <h1 className=" text-2xl font-medium text-gray-600">Courses</h1>
+          <button className=" px-3 py-2 bg-[#2196F3] text-white rounded-md hover:bg-[#1976D2]    font-semibold">
+            My Courses
+          </button>
+        </div>
+        <div className="w-full flex flex-wrap gap-2 p-4 ">
+          <DashboardCourseCard />
+          <DashboardCourseCard />
+          <DashboardCourseCard />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DashboardOverview;
