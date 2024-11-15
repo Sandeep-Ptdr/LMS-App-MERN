80378d4c38238a1d12cba3f07fc9565ed4f1488c
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-11-15 23:14:04 +0530

    first commit

--- server/src/models/transactionSchema.js
+++ server/src/models/transactionSchema.js
@@ -1,40 +1,40 @@
 import mongoose from "mongoose";
 
-
-const transactionSchema = new mongoose.Schema({
+const transactionSchema = new mongoose.Schema(
+  {
     student: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-      },
-      course: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "course",
-        required: true,
-      },
-      amount: {
-        type: Number,
-        required: true,
-      },
-      currency: {
-        type: String,
-        default: "INR",
-      },
-      transactionId: {
-        type: String,
-      },
-      orderId: {
-        type: String,
-        required: true,
-      },
-      status: {
-        type: String,
-        enum: ['created','success','failed'],
-        required: true,
-      },
-    
-    
-},  {timestamps: true})
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "course",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    currency: {
+      type: String,
+      default: "INR",
+    },
+    transactionId: {
+      type: String,
+    },
+    orderId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["created", "success", "failed"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 const Transaction = mongoose.model("Transaction", transactionSchema);
 export default Transaction;
