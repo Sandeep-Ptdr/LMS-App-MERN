147ee9805368df58c9b7fc35fa7db07406d2aff9
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-11-15 23:13:31 +0530

    update code

--- server/src/controllers/paymentController.js
+++ server/src/controllers/paymentController.js
@@ -1,9 +1,11 @@
 import { razorpayInstance } from "../config/payment.js";
 import crypto from "crypto";
 import Course from "../models/course.model.js";
+import Transaction from "../models/transactionSchema.js";
 
 const createOrder = async (req, res) => {
-  const { amount, currency = 'INR'} = req.body;
+  const { amount, currency = "INR", courseId } = req.body;
+  const userId = req.user.userInfo._id;
 
   try {
     const options = {
@@ -19,6 +21,18 @@ const createOrder = async (req, res) => {
         message: "order not created",
       });
     }
+    console.log("order", order);
+
+    const transaction = new Transaction({
+      orderId: order.id,
+      amount,
+      currency,
+      student: userId,
+      status: order.status,
+      course: courseId,
+    });
+
+    await transaction.save();
 
     res.status(200).json({
       success: true,
@@ -35,54 +49,89 @@ const createOrder = async (req, res) => {
 };
 
 const verifyPayment = async (req, res) => {
- 
-    console.log("payment details",req.body)
-    res.status(200).json({success: true, message: "Payment verified successfully and enrolled in course"})
-//   const { razorpay_payment_id, razorpay_order_id, razorpay_signature,courseId } =
-//     req.body;
+  console.log("course id", req.params.courseId);
+  console.log("payment details", req.body);
+  res
+    .status(200)
+    .json({
+      success: true,
+      message: "Payment verified successfully and enrolled in course",
+    });
+  const {
+    razorpay_payment_id,
+    razorpay_order_id,
+    razorpay_signature,
+    courseId,
+  } = req.body;
 
-//   const generatedSignature = crypto
-//     .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
-//     .update(`${razorpay_order_id}|${razorpay_payment_id}`)
-//     .digest("hex");
+  const generatedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+    .update(`${razorpay_order_id}|${razorpay_payment_id}`)
+    .digest("hex");
 
-//   if (generatedSignature === razorpay_signature) {
-//     try {
-//       const course = await Course.findById(courseId);
+  if (generatedSignature === razorpay_signature) {
+    try {
+      const transaction = await Transaction.findOneAndUpdate(
+        {
+          orderId: razorpay_order_id,
+        },
+        {
+          transactionId: razorpay_payment_id,
+          status: "success",
+        }
+      );
 
-//       if (!course) {
-//         return res.status(404).json({
-//           success: false,
-//           message: "Course not found",
-//         });
-//       }
-//       if (course.enrolledStudents.includes(req.user.userInfo._id)) {
-//         return res.status(400).json({
-//           success: false,
-//           message: "You're already enrolled in this course",
-//         });
-//       }
+      if (!transaction) {
+        return res.status(404).json({
+          success: false,
+          message: "Transaction not found",
+        });
+      }
 
-//       course.enrolledStudents.push(req.user.userInfo._id);
-//       await course.save();
+      const course = await Course.findById(courseId);
 
-//       res.status(200).json({
-//         success: true,
-//         message: "Payment verified successfully and enrolled in course",
-//       });
-//     } catch (error) {
-//       res.status(500).json({
-//         success: false,
-//         message: "Enrollment failed",
-//         error: error.message,
-//       });
-//     }
-//   } else {
-//     res.status(400).json({
-//       success: false,
-//       message: "Payment not verified",
-//     });
-//   }
+      if (!course) {
+        return res.status(404).json({
+          success: false,
+          message: "Course not found",
+        });
+      }
+      if (course.enrolledStudents.includes(req.user.userInfo._id)) {
+        return res.status(400).json({
+          success: false,
+          message: "You're already enrolled in this course",
+        });
+      }
+
+      course.enrolledStudents.push(req.user.userInfo._id);
+      await course.save();
+
+      res.status(200).json({
+        success: true,
+        message: "Payment verified successfully and enrolled in course",
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: "Enrollment failed",
+        error: error.message,
+      });
+    }
+  } else {
+    await Transaction.findByIdAndUpdate(
+      {
+        orderId: razorpay_order_id,
+      },
+      {
+        status: "failed",
+      }
+    );
+
+    res.status(400).json({
+      success: false,
+      message: "Payment not verified",
+    });
+  }
 };
 
 export { createOrder, verifyPayment };
