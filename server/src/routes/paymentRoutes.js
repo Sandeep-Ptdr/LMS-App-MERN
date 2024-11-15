a721587f052f413a0ce537ae6a18ebadf70251b1
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-11-15 23:14:35 +0530

    fix route

--- server/src/routes/paymentRoutes.js
+++ server/src/routes/paymentRoutes.js
@@ -5,6 +5,6 @@ import { createOrder, verifyPayment } from "../controllers/paymentController.js"
 const router = Router();
 
 router.post('/createorder', authenticateToken, createOrder);//for creating order    
-router.post('/verifypayment', verifyPayment);//for verify payment
+router.post('/:courseId/verifypayment', verifyPayment);//for verify payment
 
 export default router
