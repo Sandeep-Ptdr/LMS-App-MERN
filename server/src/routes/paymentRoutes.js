342a10c81e76794e3e3002821d2dd13c27ffc7d4
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-11-16 17:56:41 +0530

    fix api routes

--- server/src/routes/paymentRoutes.js
+++ server/src/routes/paymentRoutes.js
@@ -5,6 +5,6 @@ import { createOrder, verifyPayment } from "../controllers/paymentController.js"
 const router = Router();
 
 router.post('/createorder', authenticateToken, createOrder);//for creating order    
-router.post('/:courseId/verifypayment', verifyPayment);//for verify payment
+router.post('/verifypayment', authenticateToken, verifyPayment);//for verify payment
 
 export default router
