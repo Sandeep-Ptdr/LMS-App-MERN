be4735e0d74669278f63e3242f74d607e50894f4
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-11-16 17:56:12 +0530

    fix apis paths

--- server/src/routes/courseRoutes.js
+++ server/src/routes/courseRoutes.js
@@ -18,24 +18,24 @@ import { submitRating } from "../controllers/ratingController.js";
 
 const router = Router();
 
-router.post("instructor/course/create", authenticateToken, checkInstructorRole, upload.fields([{ name: "video" }, { name: "image" }]), createCourse);
-router.get("instructor/courses",authenticateToken,checkInstructorRole,getAllCourse)
-router.get("instructor/course/:courseId", authenticateToken, getCourse); //get course if student enrolled
-router.get("instructor/course/:courseId/edit", authenticateToken,checkInstructorRole, getCourse); //get course by instructor 
-router.get("instructor/course/:courseId/detail", authenticateToken,checkInstructorRole, getCourse); //get course by instructor 
-router.post("instructor/course/:courseId/enroll", authenticateToken, enrollInCourse); //enroll students in course
+router.post("/instructor/course/create", authenticateToken, checkInstructorRole, upload.fields([{ name: "video" }, { name: "image" }]), createCourse);
+router.get("/instructor/courses",authenticateToken,checkInstructorRole,getAllCourse)
+router.get("/instructor/course/:courseId", authenticateToken, getCourse); //get course if student enrolled
+router.get("/instructor/course/:courseId/edit", authenticateToken,checkInstructorRole, getCourse); //get course by instructor 
+router.get("/instructor/course/:courseId/detail", authenticateToken,checkInstructorRole, getCourse); //get course by instructor 
+router.post("/instructor/course/:courseId/enroll", authenticateToken, enrollInCourse); //enroll students in course
 router.put(
-  "instructor/:courseId/publish",
+  "/instructor/:courseId/publish",
   authenticateToken,
   checkInstructorRole,
   publishCourse
 );//only instructor publish the course
 
-router.put('instructor/course/:courseId/edit',authenticateToken,checkInstructorRole,updateCourse);//course should be update by only instructor
+router.put('/instructor/course/:courseId/edit',authenticateToken,checkInstructorRole,updateCourse);//course should be update by only instructor
 
-router.delete(`instructor/course/:courseId/delete`, authenticateToken, checkInstructorRole, deleteCourse);
+router.delete(`/instructor/course/:courseId/delete`, authenticateToken, checkInstructorRole, deleteCourse);
 
-router.post('instructor/:courseId/rate',authenticateToken,submitRating);// for giving rating
+router.post('/instructor/:courseId/rate',authenticateToken,submitRating);// for giving rating
 
 router.get('/courses',authenticateToken,getAllCourseByStudent);//get all course by student
 export default router;
