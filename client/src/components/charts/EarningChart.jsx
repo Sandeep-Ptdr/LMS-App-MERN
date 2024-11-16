7cd8d7bb5807121b5696ab695cb9eb5ee1ba9d76
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-11-16 17:53:05 +0530

    backend integration

--- client/src/components/charts/EarningChart.jsx
+++ client/src/components/charts/EarningChart.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import useFetchData from '../../hooks/useFetchData';
 
-const EarningsChart = ({ data }) => {
+const EarningsChart = () => {
+  const {data, loading, error, fetchData} = useFetchData();
+
+  useEffect(() => {
+    fetchData("/instructor/earnings","GET")
+  },[])
+
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>{error?.data?.message || "an error occured"}</p>
+  console.log('earnings',data)
+
+
+const totalEarnings = data?.earnings?.reduce((total, earning) => total + earning.earnings, 0);
+  
+
   return (
     <div className="earnings-chart border-[1px] border-gray-300 p-1 rounded-md bg-gray-50">
       <h2 className="text-xl font-semibold text-gray-600 text-center mb-4">Earnings Overview</h2>
       <ResponsiveContainer width="100%" height={300}>
-        <LineChart data={data}>
+        <LineChart data={data.earnings}>
           <CartesianGrid stroke="#e0e0e0" strokeDasharray="5 5" />
           <XAxis dataKey="month" />
           <YAxis />
