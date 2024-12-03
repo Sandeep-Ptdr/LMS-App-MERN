afc64e140b984572f5421dbf8dae5b03552d44ce
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-12-03 11:42:41 +0530

    hamburger fix

--- client/src/components/sidebars/Student.jsx
+++ client/src/components/sidebars/Student.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { RxDashboard } from "react-icons/rx";
 import { IoSearch } from "react-icons/io5";
 import { MdMenuBook } from "react-icons/md";
@@ -6,50 +6,96 @@ import { TbProgressCheck } from "react-icons/tb";
 import { HiOutlineDesktopComputer } from "react-icons/hi";
 import { BsBarChartFill } from "react-icons/bs";
 import { MdManageAccounts } from "react-icons/md";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-const Student = ({isHamburgerOpen}) => {
-  
+const Student = ({ isHamburgerOpen }) => {
   return (
-     <aside className={`h-screen w-64 bg-gray-800 text-gray-400 p-4 ${isHamburgerOpen ? 'hidden ' : 'block'}`}>
+    <aside
+      className={`h-screen w-64 bg-gray-800 text-gray-400 p-4 ${
+        isHamburgerOpen ? "hidden " : "block"
+      }`}
+    >
       <nav>
-        <ul className='flex flex-col gap-5 font-semibold text-base '>
-
-            <NavLink to='/student'>
-            <li className='cursor-pointer flex items-center gap-2'> 
-            <i><RxDashboard className='w-6 h-6'/></i> <span className='hover:text-gray-50'>Dashboard</span></li>
-            </NavLink>
-            
-
-           <NavLink to='/student/browse-courses'>
-           <li className='cursor-pointer flex items-center gap-2'> 
-           <i><IoSearch className='w-6 h-6'/></i> <span className='hover:text-gray-50'>Browse Courses</span></li>
-           </NavLink>
-
-          <NavLink to='/student/mycourses'>
-            
-          <li className='cursor-pointer flex items-center gap-2'> 
-            <i><MdMenuBook className='w-6 h-6'/></i> <span className='hover:text-gray-50'>My Courses</span></li>
+        <ul className="flex flex-col gap-5 font-semibold text-base ">
+          <NavLink to="/student"
+          end
+          className={({isActive}) => (isActive ? 'active': " ")}
+          >
+            {({isActive}) => (
+            <li className="cursor-pointer flex items-center gap-2">
+              <i>
+                <RxDashboard  className={`w-6 h-6 ${isActive ? "text-[#2196F3]":"text-gray-300"}`} />
+              </i>{" "}
+              <span className="hover:text-gray-50">Dashboard</span>
+            </li>
+            )}
           </NavLink>
 
-            <li className='cursor-pointer flex items-center gap-2'> 
-            <i><TbProgressCheck className='w-6 h-6'/></i> <span className='hover:text-gray-50'>Completed Courses</span></li>
+          <NavLink to="/student/browse-courses"
+          className={({isActive}) => (isActive ? 'active': " ")}
+          >
+            {({isActive}) => (
+            <li className="cursor-pointer flex items-center gap-2">
+              <i>
+                <IoSearch  className={`w-6 h-6 ${isActive ? "text-[#2196F3]":"text-gray-300"}`} />
+              </i>{" "}
+              <span className="hover:text-gray-50">Browse Courses</span>
+            </li>
+            )}
+          </NavLink>
 
-            <li className='cursor-pointer flex items-center gap-2'> 
-            <i>< HiOutlineDesktopComputer className='w-6 h-6'/></i> <span className='hover:text-gray-50'>Take Quiz</span></li>
-
-            <li className='cursor-pointer flex items-center gap-2'> 
-            <i><BsBarChartFill className='w-6 h-6'/></i> <span className='hover:text-gray-50'>Quiz Results</span></li>
-
-            <li className='cursor-pointer flex items-center gap-2'> 
-            <i><MdManageAccounts  className='w-6 h-6'/></i> <span className='hover:text-gray-50'>Account</span>
+          <NavLink to="/student/mycourses"
+          className={({isActive}) => (isActive ? 'active': " ")}
+          >
+            {({isActive}) => (
             
-            </li> 
+            <li className="cursor-pointer flex items-center gap-2">
+              <i>
+                <MdMenuBook className={`w-6 h-6 ${isActive ? "text-[#2196F3]":"text-gray-300"}`} />
+              </i>{" "}
+              <span className="hover:text-gray-50">My Courses</span>
+            </li>
+            )}
+          </NavLink>
+
+          <li className="cursor-pointer flex items-center gap-2">
+            <i>
+              <TbProgressCheck className="w-6 h-6" />
+            </i>{" "}
+            <span className="hover:text-gray-50">Completed Courses</span>
+          </li>
+
+          <NavLink to="/student/course/lesson/1/quiz"
+          className={({isActive}) => (isActive ? 'active': " ")}
+          >
+            {({isActive}) => (
             
+            <li className="cursor-pointer flex items-center gap-2">
+              <i>
+                <HiOutlineDesktopComputer className={`w-6 h-6 ${isActive ? "text-[#2196F3]":"text-gray-300"}`} />
+              </i>{" "}
+              <span className="hover:text-gray-50">Take Quiz</span>
+            </li>
+            )}
+          </NavLink>
+
+          <li className="cursor-pointer flex items-center gap-2">
+            <i>
+              <BsBarChartFill className="w-6 h-6" />
+            </i>{" "}
+            <span className="hover:text-gray-50">Quiz Results</span>
+          </li>
+
+          <li className="cursor-pointer flex items-center gap-2">
+            <i>
+              <MdManageAccounts className="w-6 h-6" />
+            </i>{" "}
+            <span className="hover:text-gray-50">Account</span>
+          </li>
         </ul>
       </nav>
-     </aside>
-  )
-}
+    </aside>
+  );
+};
 
-export default Student
+export default Student;
