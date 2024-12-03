b214f88bc1acf8027a67d1bde099456cf85d0c63
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-12-03 11:43:21 +0530

    code clean up

--- client/src/layouts/InstructorLayout.jsx
+++ client/src/layouts/InstructorLayout.jsx
@@ -4,21 +4,19 @@ import Instructor from "../components/sidebars/Instructor";
 import { useState } from "react";
 
 const InstructorLayout = () => {
-  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false)
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
 
   const hamburger = () => {
     setIsHamburgerOpen(!isHamburgerOpen);
-    
-     
-  }
+  };
 
   return (
     <>
       <div className="flex flex-col h-screen">
         <Header setHamburger={hamburger} />
         <div className="flex flex-1 overflow-hidden">
-        {/* sidebar */}
-          <Instructor isHamburgerOpen={isHamburgerOpen}  /> 
+          {/* sidebar */}
+          <Instructor isHamburgerOpen={isHamburgerOpen} />
 
           <main className="flex flex-1 overflow-y-auto w-[calc(100vw-256px)] bg-gray-200">
             <Outlet />
