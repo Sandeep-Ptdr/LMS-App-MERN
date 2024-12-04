530a34ac797ac5958274f288079265614dfe1129
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-12-04 22:37:14 +0530

    improve security and logout functionality

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import {jwtDecode} from "jwt-decode";
 
 
-const ProtectedRoute = ({children}) => {
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("authToken");
 
-     
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
 
-    const isAuthenticated = Boolean(localStorage.getItem("authToken"));
+  try {
+    const decodeToken = jwtDecode(token);
 
-    return isAuthenticated ? children :<Navigate to="/login" /> ;
-}
+    console.log("decodetoken", decodeToken);
+
+    const currentTime = Date.now() / 1000;
+
+    if (decodeToken.exp < currentTime) {
+      localStorage.removeItem("authToken");
+      return <Navigate to="/login" />;
+    }
+  } catch (error) {
+    localStorage.removeItem("authToken");
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
 
 export default ProtectedRoute;
