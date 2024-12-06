adf7956d487a69a77d9454c91ca72e6c31544bfc
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-12-06 21:24:39 +0530

    fetch data of user

--- client/src/pages/dashboard/studentDashboard/StudentDashboard.jsx
+++ client/src/pages/dashboard/studentDashboard/StudentDashboard.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import DashboardOverview from '../../../components/dashboard/student/DashboardOverview'
+import useFetchData from '../../../hooks/useFetchData'
 
 const StudentDashboard = () => {
+  
+   const {data,loading,error,fetchData}= useFetchData()
+
+  useEffect(() => {
+
+    fetchData('/student','GET')
+
+  },[])
+
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>{error?.data?.message || "an error occured"}</p>
+
   return (
     <div className="container mx-auto p-4">
       <div className=" max-w-[95%] m-auto">
-         <DashboardOverview />
+         <DashboardOverview  data={data}/>
       </div>
     </div>
   )
