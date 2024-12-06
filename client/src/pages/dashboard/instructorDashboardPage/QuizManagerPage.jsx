7b2bc30fe3c4b8cce1e89ce8dba3df9dffea6b3e
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-12-06 21:01:03 +0530

    update code

--- client/src/pages/dashboard/instructorDashboardPage/QuizManagerPage.jsx
+++ client/src/pages/dashboard/instructorDashboardPage/QuizManagerPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import QuizManager from '../../../components/dashboard/instructor/course/QuizManager'
+import CreateQuiz from '../../../components/dashboard/instructor/course/CreateQuiz.jsx'
 
 const QuizManagerPage = () => {
   return (
     <div className="w-full p-4">
     <div className=" max-w-[95%] m-auto">
-        <QuizManager/>
+         <CreateQuiz/>
     </div>
   </div>
   )
