4798716c5237091ca68ebf07a5b664031f3bbe67
Sandeep Patidar <patidarsandeep309@gmail.com>
2024-12-08 08:40:11 +0530

    set error

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -4,8 +4,9 @@ import API from "../../utils/api";
 import image from "../../../src/assets/image.png"
 
 const Login = () => {
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({ email: "", password: "" });
-  const [error, setError] = useState("");
+  const [error, setError] = useState (null)
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -16,19 +17,32 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      setLoading(true);
       const { data } = await API.post("/auth/login", formData);
       localStorage.setItem("authToken", data.accessToken);
       if(data?.user.role === "instructor") navigate("/instructor");
       else navigate("/student");
     } catch (error) {
       console.error(error);
+      setError(error)
+    } finally{
+      setLoading(false)
     }
   };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100  ">
       
-      <div className="w-full max-w-md bg-gray-50 p-8 shadow-md rounded-md">
+      {loading && (
+        <div className="absolute inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-10">
+          <div className="loader"></div>
+        </div>
+      )}
+      <div
+        className={`w-full max-w-md bg-gray-50 p-8 shadow-md rounded-md ${
+          loading ? "opacity-50" : "opacity-100"
+        }`}
+      >
         <div className="border-b-[1px] pb-2">
           <h2 className="font-bold text-center text-2xl text-gray-700">
             Login
@@ -54,7 +68,7 @@ const Login = () => {
             />
           </div>
 
-          <div className="mb-4 flex flex-col">
+          <div className="mb-2 flex flex-col">
             <label
               htmlFor="password"
               className="block text-lg text-gray-600 mb-1"
@@ -72,6 +86,9 @@ const Login = () => {
             />
           </div>
 
+          {error && (
+            <p className="text-red-500 text-sm mb-2">{error?.response?.data?.error}</p>
+          )}
           <button
             type="submit"
             className="bg-[#2196F3] text-white rounded-lg py-2 text-lg font-semibold hover:bg-[#348dd6] transition duration-300 ease-in-out"
