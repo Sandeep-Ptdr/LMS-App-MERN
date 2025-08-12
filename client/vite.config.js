76eefa1a2faab371ab58d10578e8063d6eca9961
Sandeep Patidar <patidarsandeep309@gmail.com>
2025-08-12 22:30:23 +0530

    add config for vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
     proxy: {
       '/api/v1' : 'http://localhost:3000',
